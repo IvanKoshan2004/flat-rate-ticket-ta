de3ed045ba213ed32553f90c38a3cb43c05e746f
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-07-30 16:55:05 +0300

    separated the server into a different file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,4 @@
-import express from "express";
-import router from "./routes/router";
-const app = express();
-
-app.use(router);
-app.listen(3000, () => {
+import server from "./server";
+server.listen(3000, () => {
     console.log("Service listening on port 3000");
 });
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import router from "./routes/router";
+const server = express();
+server.use(router);
+export default server;
