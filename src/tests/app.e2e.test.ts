1e4c97ccef92da787e1597d2b2e1eb24866a4f48
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-07-31 20:58:16 +0300

    added graphql test suite

--- src/tests/app.e2e.test.ts
+++ src/tests/app.e2e.test.ts
@@ -4,7 +4,7 @@ import supertest from "supertest";
 import { Express } from "express";
 jest.setTimeout(10000); //for slow connections
 jest.retryTimes(3);
-describe("Endpoint 2e2 testing", () => {
+describe("REST endpoint 2e2 testing", () => {
     let app!: Express;
     beforeEach(async () => {
         app = await createServer();
@@ -34,5 +34,51 @@ describe("Endpoint 2e2 testing", () => {
     it("Should respond with Bad Request (code: 400) if the id query param is unspecified", async () => {
         const res = await supertest(app).get(`/api`);
         expect(res.statusCode).toEqual(400);
+    });
+});
+describe("GraphQL endpoint 2e2 testing", () => {
+    let app!: Express;
+    beforeEach(async () => {
+        app = await createServer();
+    });
+    it("getAllTickets should respond with an array of Tickets, if the event with id exists", async () => {
+        const query = `
+        {
+            getAllTickets(id: 1195) {
+                row
+                price
+                seatNumber
+                section
+            }
+        }
+        `;
+        const res = await supertest(app).post(`/graphql?query=${query}`);
+        const tickets = res.body.data.getAllTickets as Ticket[];
+        expect(Array.isArray(tickets)).toBeTruthy();
+        tickets.forEach((ticket) => {
+            expect(ticket.row).toEqual(expect.any(String));
+            expect(ticket.price).toEqual(expect.any(Number));
+            expect(ticket.section).toEqual(expect.any(String));
+            expect(ticket.seatNumber).toEqual(expect.any(Number));
+        });
+    });
+    it("getTicketsWithPriceLessThan should respond with tickets with price less than number", async () => {
+        const price = Math.round((Math.random() + 0.5) * 1000);
+        const query = `
+        {
+            getTicketsWithPriceLessThan(id: 1195, price: ${price}) {
+                row
+                price
+                seatNumber
+                section
+            }
+        }
+        `;
+        const res = await supertest(app).post(`/graphql?query=${query}`);
+        const tickets = res.body.data.getTicketsWithPriceLessThan as Ticket[];
+        const ticketWithPriceAbove = tickets.findIndex(
+            (ticket) => ticket.price > price
+        );
+        expect(ticketWithPriceAbove).toEqual(-1);
     });
 });
